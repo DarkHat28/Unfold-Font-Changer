11acd8b9d2bc14addb367bc37a134743e8673b64
Dark Hat <140172956+DarkHat28@users.noreply.github.com>
2023-08-30 16:03:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/DarkHat28/Unfold-Font-Changer.git /Unfold-Font-Changer
-cd /Stylish-Text-Bot
+cd /Unfold-Font-Changer
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
